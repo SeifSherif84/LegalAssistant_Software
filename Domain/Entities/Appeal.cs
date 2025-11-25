b967d6db4d6022bdaddc927add577062e39e4c12
Wageh_Mostafa <120650371+wageh3@users.noreply.github.com>
2025-11-25 23:13:39 +0200

    Appeal , Decision, Document

---
+++ Domain/Entities/Appeal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Appeal
+    {
+        public int Id { get; set; }
+        public DateTime FiledDate { get; set; }
+        public string AppealType { get; set; }
+        public string Status { get; set; }
+        public int CaseId { get; set; }
+        public Case Case { get; set; }
+    }
+}
---
+++ Domain/Entities/Decision.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Decision
+    {
+        public int Id { get; set; }
+        public DateTime DecisionDate { get; set; }
+        public string DecisionType { get; set; }
+        public bool Appealable { get; set; }
+        public string DecisionText { get; set; }
+        public DateTime AppealDeadline { get; set; }
+        public Case Case { get; set; }
+
+    }
+}
