b89cd3b226025415a2745433503413ccd3ee5857
Wageh_Mostafa <120650371+wageh3@users.noreply.github.com>
2025-11-26 00:52:22 +0200

    move enums to enums folder

--- Domain/Entities/Lawyer.cs
+++ Domain/Entities/Lawyer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Entities.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,12 +12,5 @@ namespace Domain.Entities
         public Specialties Specialty { get; set; }
         public int YearsOfExperience { get; set; }
         public decimal HourlyRate { get; set; }
-
-
-        public List<Case> Cases { get; set; } // Many-to-Many relationship with Case
-
-
-        public int OfficeId { get; set; }
-        public Office Office { get; set; } // Many-to-One relationship with Office
     }
 }
