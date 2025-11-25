4a61707ca55ce2fa7c4a231103f4dfc8bdb4797e
Wageh_Mostafa <120650371+wageh3@users.noreply.github.com>
2025-11-25 22:46:36 +0200

    Case Entity

---
+++ Domain/Entities/Case.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Case
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int FileNumber { get; set; }
+        public string CourtName { get; set; }
+        public string Status { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+        public DateTime NextHearingDate { get; set; }
+        public DateTime VerdictDate { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
